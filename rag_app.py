cae36b6438f6e9ced3fe31c7f5c7506e46303d0b
VTaPo <vtphong82@gmail.com>
2024-11-09 11:50:32 +0700

    Upload code

---
+++ rag_app.py
@@ -0,0 +1,102 @@
+from typing import List, Dict, Any
+import requests
+import os
+import re
+from langchain_openai import OpenAIEmbeddings
+from pydantic import BaseModel, Field
+from dotenv import load_dotenv
+from rag_search import search_google, highlight_citations, ReportGenerator
+
+import streamlit as st
+
+def main():
+    os.environ['OPENAI_API_KEY'] = st.secrets["OPENAI_API_KEY"]
+    gg_api_key = st.secrets["GG_API_KEY"]
+    gg_cse_id = st.secrets["CSE_ID"]
+
+    # Initialize OpenAI embeddings
+    embeddings = OpenAIEmbeddings(
+        model="text-embedding-3-large",
+        dimensions=1024
+    )
+
+    # Webpage Logo
+    col1, col2, col3 = st.columns([0.35, 0.3, 0.35])
+    with col2:
+        st.markdown(
+            """
+            <style>
+            .center-image {
+                display: flex;
+                justify-content: center;
+            }
+            </style>
+            """,
+            unsafe_allow_html=True
+        )
+        with st.container():
+            st.markdown('<div class="center-image">', unsafe_allow_html=True)
+            st.image("banner.png", width=180)
+            st.markdown('</div>', unsafe_allow_html=True)
+    
+    # Web Title
+    st.markdown("<h1 style='text-align: center; margin-top: 0;'>EdSearch</h1>", unsafe_allow_html=True)
+
+    # Initialize session state for topic and output_len if they don't exist
+    if "topic" not in st.session_state:
+        st.session_state["topic"] = ""
+    if "output_len" not in st.session_state:
+        st.session_state["output_len"] = "1000"
+    
+    # Input columns
+    col1, col2 = st.columns([3, 1])
+
+    # Topic box on the left column
+    with col1:
+        st.session_state["topic"] = st.text_input("Enter Topic:", st.session_state["topic"])
+    
+    # Length box on the right column
+    with col2:
+        st.session_state["output_len"] = st.text_input("~Length:", st.session_state["output_len"])
+    
+    # Search and Reset buttons
+    col_search, col_reset = st.columns([0.3, 0.3])
+    
+    with col_search:
+        search_clicked = st.button("Search")
+
+    with col_reset:
+        reset_clicked = st.button("Reset")
+    
+    # Handle Search button click
+    if search_clicked:
+        if st.session_state["topic"] and st.session_state["output_len"].isdigit():
+            # Generate the article
+            # Initialize report generator
+            report_gen = ReportGenerator(embeddings, st.session_state["output_len"])
+
+            #Retrieve documents from Google search
+            docs = search_google(st.session_state["topic"], gg_api_key, gg_cse_id)
+
+            # Generate report
+            result = report_gen.generate_report(st.session_state["topic"], docs)
+
+            st.session_state.article = result["report"]
+            
+            # Show the generated article
+            with st.expander("Click to View full Article"):
+                st.markdown(result["report"], unsafe_allow_html=True)
+        else:
+            st.warning("Please enter a valid topic and word count.")
+
+    # Handle Reset button click
+    if reset_clicked:
+        # del st.session_state["topic"]
+        # del st.session_state["output_len"]
+        if 'article' in st.session_state:
+            del st.session_state["article"]
+        st.session_state["topic"] = ""
+        st.session_state["output_len"] = "1000"
+
+if __name__ == "__main__":
+    main()
---
+++ rag_search.py
@@ -0,0 +1,182 @@
+from typing import List, Dict, Any
+import requests
+import os
+import re
+from pydantic import BaseModel, Field
+
+import googlesearch
+from trafilatura import fetch_url, extract
+
+from langchain_core.runnables import RunnableSequence, RunnablePassthrough
+from langchain.prompts import PromptTemplate, ChatPromptTemplate
+from langchain.schema import Document
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from dotenv import load_dotenv
+
+def highlight_citations(markdown_text):
+    highlighted_text = re.sub(r'\((\d+)\)', r'<span style="color:blue;">(\1)</span>', markdown_text)
+    return highlighted_text
+
+def remove_citations_and_links(text: str) -> str:
+    # Remove [number] citations
+    text = re.sub(r'\[\d+\]', '', text)
+    # Remove URLs
+    text = re.sub(r'https?://\S+', '', text)
+    return text
+
+def search_google(query, api_key, cse_id):
+    # URL endpoint of Google Custom Search API
+    url = 'https://www.googleapis.com/customsearch/v1'
+    
+    # Parameters for the API request
+    params = {
+        'q': query,            # Query topic
+        'key': api_key,        # API Key
+        'cx': cse_id,          # Custom Search Engine ID
+        'num': 4               # The number of search results to return
+    }
+    
+    # Send GET request to the API
+    response = requests.get(url, params=params)
+    
+    # Check if the request is successful
+    if response.status_code == 200:
+        search_results = response.json()
+        results = search_results.get('items', [])  # Get the search results
+        webs = []
+        for res in results:
+            _dict = {}
+            _dict['url'] = res['link']
+            _dict['content'] = remove_citations_and_links(extract(fetch_url(res['link'])))
+            webs.append(_dict)
+        return {"topic":query,"context":webs}
+    else:
+        return None
+    
+class CitedAnswer(BaseModel):
+    """Answer the user question based only on the given sources, and cite the sources used."""
+
+    answer: str = Field(
+        ...,
+        description="The answer to the user query, which is based only on the given sources.",
+    )
+    citations: List[int] = Field(
+        ...,
+        description="The integer IDs of the SPECIFIC sources which justify the answer.",
+    )
+
+class ReportGenerator:
+    def __init__(self, embeddings, len, model_name: str = "gpt-4o-mini"):
+        """Initialize the Text Generator"""
+        self.embeddings = embeddings
+        self.llm = ChatOpenAI(
+            model=model_name,
+            temperature=0.2,
+        )
+        self.len = len
+        self.report_chain = self._create_report_chain()
+
+    def _create_report_chain(self):
+        """Create the report generation chain."""
+        report_template = """
+        Use only the contents in all <Docs> sections of 'Available Documents' to write a narrative story about {query}.
+        **Do not create or add 'In Conclusion' or 'In Summary'.**
+        **Do not include any information or sections not provided or not accurate.**
+        **Cite sources using (number) format directly in the text.**
+
+        **Required Length:** The story should be approximately **{len} words** (within ±10 percents of this length).
+        Write in **MARKDOWN FORMAT**.
+        The story should focus primarily on three sections: **'History'**, **'Applications'**, and **'Common tasks'**. 
+        - If necessary to reach the target length, you may add relevant sections from the provided documents.
+        - Do not create content beyond the provided documents to extend the length.
+
+        Formatting Guidelines:
+        - The highest level of headings is level 2 (##).
+        - Bold the most relevant terms or ideas where helpful.
+        - Keep content accurate, factual, and without additions or extrapolated conclusions.
+
+        **Available Documents:**
+        {formatted_docs}
+
+        STORY:
+        """
+        
+        report_prompt = PromptTemplate(
+            template=report_template,
+            input_variables=["query", "formatted_docs", "len"]
+        )
+        structured_llm = self.llm.with_structured_output(CitedAnswer)
+        report_chain = (
+            RunnablePassthrough.assign(formatted_docs=(lambda x: self._format_documents(x["context"]))).assign(
+                len=(lambda x: self.len)
+            )
+            | report_prompt
+            | structured_llm
+        )
+        return  report_chain
+
+    def _format_documents(self, docs: dict) -> str:
+        """Format documents for LLM consumption with citations."""
+        texts = docs["context"]
+        formatted_docs = []
+        texts = [doc['content'] for doc in texts]
+        for i, doc in enumerate(texts, 1):
+            source_info = f"[Doc{i}] "
+            formatted_docs.append(
+                f"<{source_info}>\n{doc}\n</{source_info}>"
+            )
+        return "\n\n".join(formatted_docs)
+
+    def _format_citations(self, docs: List[dict], index: List[int]) -> str:
+        """Create detailed citations for the sources section."""
+        citations = []
+
+        for i in index:
+            metadata = docs[i-1]['url']
+            citation = f"[({i})]"
+
+            citation += f"({metadata})"
+            
+            citations.append(citation)
+        
+        return " ".join(citations)
+
+    def generate_report(self, question: str, srcs: dict) -> Dict[str, Any]:
+        """
+        Generate a report answering the question using retrieved documents.
+        
+        Args:
+            question: The research question to answer
+            k: Number of documents to retrieve
+            
+        Returns:
+            Dict containing the report and metadata
+        """
+        try:
+            chain_input = {"query": question, "context": srcs}
+            report_result = self.report_chain.invoke(chain_input)
+            # Add sources section
+            final_report = report_result.dict()
+            index = final_report['citations']
+            print(index)
+            ans = final_report['answer']
+            # new_index, miss_values = re_index(index)
+            # print(miss_values)
+            # for i in range(len(miss_values)):
+            #     need_repl, repl = miss_values[i][0], miss_values[i][1]
+            #     ans = ans.replace(f'[{need_repl}]', f'[{repl}]')
+
+            full_report = f"{ans}\n\nReferences:\n{self._format_citations(srcs["context"], index)}"
+            full_report = f'''# {question.upper()}\n\n'''+full_report 
+            return {
+                "report": full_report,
+                "metadata": {
+                    "topic:": question,
+                    "model": self.llm.model_name,
+                }
+            }
+            
+        except Exception as e:
+            print(f"Error generating report: {str(e)}")
+            raise
+
